12695a9f06adbae04e7fd83d5533c9814cb32ff9
roman <roman.a.shushakov1@gmail.com>
2026-05-03 18:56:40 +0200

    removed rag_indexer_jetson

--- rag_indexer_jetson/src/types.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct ChunkEmbedding {
-    pub doc_id: String,
-    pub chunk_id: String,
-    pub embedding: Vec<f32>,
-}
